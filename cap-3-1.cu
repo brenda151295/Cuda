ab229c3c796066abe528c185abf045ac1f012e90
brenda solari <brenda.solari@ucsp.edu.pe>
2016-01-31 16:45:40 -0500

    first commit

---
+++ cap-3-1.cu
@@ -0,0 +1,117 @@
+/*
+ * Compile:  nvcc [-g] [-G] -arch=sm_21 -o mat_add mat_add.cu 
+ * Run:      ./mat_add <m> <n>
+ *              m is the number of rows
+ *              n is the number of columns
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define BLOCK_DIM 512
+
+/*
+[1 1 1]
+[1 1 1]    =>   [1 1 1][1 1 1][1 1 1]
+[1 1 1]
+
+*/
+
+/*Kernel*/
+__global__ void matrixAdd(float a[], float b[], float c[], int N) {
+   int col = blockIdx.x * blockDim.x + threadIdx.x;
+   int row = blockIdx.y * blockDim.y + threadIdx.y;
+   int index = col + row * N;
+   if (col < N && row < N) {
+      c[index] = a[index] + b[index];
+ }
+}
+__global__ void matrixAddRow(float a[], float b[], float c[], int N) {
+   int row = blockIdx.y * blockDim.y + threadIdx.y;
+   int index;
+   for(index = row * N; index < N; index++)
+   {
+      c[index] = a[index] + b[index];
+   }
+}
+__global__ void matrixAddColumn(float a[], float b[], float c[], int N) {
+   int col = blockIdx.x * blockDim.x + threadIdx.x;
+   int index;
+   for(index = col * N; index < N; index = index + N)
+   {
+      c[index] = a[index] + b[index];
+   }
+}
+
+
+
+
+void Read_matrix(float A[], int s) {
+   int i, j;
+
+   for (i = 0; i < s; i++)
+      for (j = 0; j < s; j++)
+         scanf("%f", &A[i*s+j]);
+}
+void Print_matrix(char name[], float A[], int s) {
+   int i, j;
+
+   printf("%s\n", name);
+   for (i = 0; i < s; i++) {
+      for (j = 0; j < s; j++)
+         printf("%.1f ", A[i*s+j]);
+      printf("\n");
+   }  
+}
+/* Host */
+int main(int argc, char* argv[]) {
+   int N;
+   int size;
+   float *dev_a, *dev_b, *dev_c;
+   float *a, *b, *c;
+
+   N = strtol(argv[1], NULL, 10);
+   printf("size = %d", N);
+
+   size = N*N*sizeof(float);
+
+
+   a = (float*) malloc(size);
+   b = (float*) malloc(size);
+   c = (float*) malloc(size); 
+
+   printf("Matriz A: \n");
+   Read_matrix(a, N);
+   printf("Matriz B: \n");
+   Read_matrix(b, N);
+
+   Print_matrix("A =", a, N);
+   Print_matrix("B =", b, N);
+
+
+   cudaMalloc(&dev_a, size);
+   cudaMalloc(&dev_b, size);
+   cudaMalloc(&dev_c, size);
+
+   cudaMemcpy(dev_a, a, size, cudaMemcpyHostToDevice);
+   cudaMemcpy(dev_b, b, size, cudaMemcpyHostToDevice);
+
+   dim3 dimBlock(BLOCK_DIM, BLOCK_DIM);
+   dim3 dimGrid((int)ceil(N/dimBlock.x),(int)ceil(N/dimBlock.y));
+
+   matrixAdd<<<dimGrid,dimBlock>>>(dev_a,dev_b,dev_c,N);
+   /* Wait for the kernel to complete */
+   cudaThreadSynchronize();
+
+   cudaMemcpy(c, dev_c, size, cudaMemcpyDeviceToHost);
+   Print_matrix("Result =", c, N);
+
+   cudaFree(dev_a);
+   cudaFree(dev_b);
+   cudaFree(dev_c);
+
+   free(a);
+   free(b);
+   free(c);
+
+   return 0;
+}
---
+++ cap-3-2.cu
@@ -0,0 +1,120 @@
+/*
+ * Compile:  nvcc [-g] [-G] -arch=sm_21 -o mat_add mat_add.cu 
+ * Run:      ./mat_add <m> <n>
+ *              m is the number of rows
+ *              n is the number of columns
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define BLOCK_DIM 512
+
+/*
+[1 1 1]
+[1 1 1]    =>   [1 1 1][1 1 1][1 1 1]
+[1 1 1]
+
+*/
+
+/*Kernel*/
+/*a is a matrix
+b is a vector
+c is a result (vector)*/
+__global__ void matrixVectMult(float a[], float b[], float c[], int N) {
+   int col = blockIdx.x * blockDim.x + threadIdx.x;
+   int row = blockIdx.y * blockDim.y + threadIdx.y;
+   int index = col + row * N;
+   if (col < N && row < N) {
+      c[row] = a[index] + b[row];
+ }
+}
+
+void Read_matrix(float A[], int s) {
+   int i, j;
+
+   for (i = 0; i < s; i++)
+      for (j = 0; j < s; j++)
+         scanf("%f", &A[i*s+j]);
+}
+
+void Read_vector(float A[], int s) {
+   int i;
+
+   for (i = 0; i < s; i++)
+      scanf("%f", &A[i]);
+}
+
+void Print_matrix(char name[], float A[], int s) {
+   int i, j;
+
+   printf("%s\n", name);
+   for (i = 0; i < s; i++) {
+      for (j = 0; j < s; j++)
+         printf("%.1f ", A[i*s+j]);
+      printf("\n");
+   }  
+}
+void Print_vector(char name[], float A[], int s) {
+   int i;
+
+   printf("%s\n", name);
+   for (i = 0; i < s; i++) {
+      printf("%.1f ", A[i]);
+      printf("\n");
+   }  
+}
+/* Host */
+int main(int argc, char* argv[]) {
+   int N;
+   int size_matrix;
+   int size_vector;
+   float *dev_a, *dev_b, *dev_c;
+   float *a, *b, *c;
+
+   N = strtol(argv[1], NULL, 10);
+   printf("size = %d", N);
+
+   size_matrix = N*N*sizeof(float);
+   size_vector = N*sizeof(float);
+
+
+   a = (float*) malloc(size_matrix);
+   b = (float*) malloc(size_vector);
+   c = (float*) malloc(size_vector); 
+
+   printf("Matrix A: \n");
+   Read_matrix(a, N);
+   printf("Vector B: \n");
+   Read_vector(b, N);
+
+   Print_matrix("A =", a, N);
+   Print_vector("B =", b, N);
+
+
+   cudaMalloc(&dev_a, size_matrix);
+   cudaMalloc(&dev_b, size_vector);
+   cudaMalloc(&dev_c, size_vector);
+
+   cudaMemcpy(dev_a, a, size_matrix, cudaMemcpyHostToDevice);
+   cudaMemcpy(dev_b, b, size_vector, cudaMemcpyHostToDevice);
+
+   dim3 dimBlock(BLOCK_DIM, BLOCK_DIM);
+   dim3 dimGrid((int)ceil(N/dimBlock.x),(int)ceil(N/dimBlock.y));
+
+   matrixVectMult<<<dimGrid,dimBlock>>>(dev_a,dev_b,dev_c,N);
+   /* Wait for the kernel to complete */
+   cudaThreadSynchronize();
+
+   cudaMemcpy(c, dev_c, size_matrix, cudaMemcpyDeviceToHost);
+   Print_vector("Result =", c, N);
+
+   cudaFree(dev_a);
+   cudaFree(dev_b);
+   cudaFree(dev_c);
+
+   free(a);
+   free(b);
+   free(c);
+
+   return 0;
+}
